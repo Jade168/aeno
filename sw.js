821168cefc9526a842d70c4fd3341e4e29831ae3
Jade168 <128027202+Jade168@users.noreply.github.com>
2026-02-14 19:28:02 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,39 @@
-// AENO SW v3 - FORCE UPDATE (NO CACHE)
+const CACHE_NAME = "aeno-v3-cache-3000";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.json"
+];
 
 self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request, { cache: "no-store" }).catch(() => {
-      return new Response("Offline - AENO cannot load", {
-        status: 503,
-        headers: { "Content-Type": "text/plain" },
-      });
-    })
+    fetch(event.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      return res;
+    }).catch(() => caches.match(event.request))
   );
 });
