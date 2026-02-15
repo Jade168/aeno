f86a7668fb6a6993fd990fe55779c8f5f887a22f
Jade168 <128027202+Jade168@users.noreply.github.com>
2026-02-15 14:03:53 +0800

    Update service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,21 +1,31 @@
-// sw.js - AENO FIX VERSION
+const CACHE_NAME = "aeno-v3-cache-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./characters.js",
+  "./ads.json",
+  "./manifest.json"
+];
+
 self.addEventListener("install", (event) => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    (async () => {
-      const keys = await caches.keys();
-      for (const k of keys) {
-        await caches.delete(k);
-      }
-      await self.clients.claim();
-    })()
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
   );
 });
 
-// NEVER cache anything (development safe)
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
 });
